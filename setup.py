7111ec93c69abeffc0a682b326b9417feafeb4ad
JungYoon Cha <jamiecha@gmail.com>
2021-09-07 19:10:16 +0900

    fixed README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jsonpatch2pymongo",
-    version="0.1.0",
+    version="0.1.1",
     author="JungYoon Cha",
     author_email="jamiecha@gmail.com",
     description="Convert JSON patch into pymongo",
