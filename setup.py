0f784c8b660fec86da6fb63e29008c057bbfb86c
JungYoon Cha <jamiecha@gmail.com>
2021-12-16 14:08:40 +0900

    Added github actions

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+
+def get_version():
+    version_file = "src/jsonpatch2pymongo/_version.py"
+    with open(version_file) as f:
+        exec(compile(f.read(), version_file, "exec"))
+    return locals()["__version__"]
+
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="jsonpatch2pymongo",
-    version="0.1.1",
+    version=get_version(),
     author="JungYoon Cha",
     author_email="jamiecha@gmail.com",
     description="Convert JSON patch into pymongo",
